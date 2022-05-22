3a6ac02543f57b0b9013fc09ec9d4a4cb8a1879e
raylicola <rope.champ.109@gmail.com>
2022-05-22 15:50:26 +0900

    DB初期データ挿入

--- docker/mysql/initdb.d/1_create_table.sql
+++ docker/mysql/initdb.d/1_create_table.sql
@@ -2,11 +2,54 @@ set global local_infile = 1;
 
 USE nflaquiz;
 
-CREATE TABLE employee
+CREATE TABLE area
 (
-  id           INT(10),
-  name     VARCHAR(40)
+  id VARCHAR(30),
+  name VARCHAR(10),
+  PRIMARY KEY(id)
 );
 
-INSERT INTO employee (id, name) VALUES (1, "Nagaoka");
-INSERT INTO employee (id, name) VALUES (2, "Tanaka");
+CREATE TABLE color
+(
+  id VARCHAR(10),
+  name VARCHAR(10),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE country
+(
+  id VARCHAR(2),
+  area_id VARCHAR(30),
+  name VARCHAR(20),
+  description VARCHAR(200),
+  FOREIGN KEY (area_id) REFERENCES area(id),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE flag_color
+(
+  id INT NOT NULL AUTO_INCREMENT UNIQUE,
+  country_id VARCHAR(2),
+  color_id VARCHAR(10),
+  FOREIGN KEY (country_id) REFERENCES country(id),
+  FOREIGN KEY (color_id) REFERENCES color(id),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE quiz
+(
+  id INT NOT NULL AUTO_INCREMENT UNIQUE,
+  hiragana VARCHAR(30),
+  country_id VARCHAR(2),
+  FOREIGN KEY (country_id) REFERENCES country(id),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE hint
+(
+  id INT NOT NULL AUTO_INCREMENT UNIQUE,
+  content VARCHAR(200),
+  country_id VARCHAR(2),
+  FOREIGN KEY (country_id) REFERENCES country(id),
+  PRIMARY KEY(id)
+);
