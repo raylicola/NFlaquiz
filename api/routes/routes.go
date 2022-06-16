7c76eb03f5481ba0e3574795cdf5b979cd13e0a9
raylicola <rope.champ.109@gmail.com>
2022-06-16 21:03:21 +0900

    Vueからのアクセスを許可

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,12 +1,33 @@
 package routes
 
 import (
+	"time"
 	"github.com/raylicola/NFlaquiz/controllers"
 	"github.com/gin-gonic/gin"
+  "github.com/gin-contrib/cors"
 )
 
 func GetRouter() *gin.Engine {
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		// 許可するアクセス元
+		AllowOrigins: []string{
+				"http://localhost:8080",
+		},
+		// アクセス許可するHTTPメソッド
+		AllowMethods: []string{
+				"POST",
+				"GET",
+		},
+		// 許可するHTTPリクエストヘッダ
+		AllowHeaders: []string{
+				"Content-Type",
+		},
+		// cookieなどの情報を必要とする
+		AllowCredentials: true,
+		// preflightリクエストの結果をキャッシュする時間
+		MaxAge: 24 * time.Hour,
+}))
 
 	router.POST("/signup", controllers.Signup)
 	router.POST("/login", controllers.Login)
