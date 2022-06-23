e3df807ba6aba6213a6bac5d55758b7085e45002
raylicola <rope.champ.109@gmail.com>
2022-06-23 11:53:19 +0900

    マップ情報をGetで返す処理の作成

--- api/controllers/top_controller.go
+++ api/controllers/top_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -13,14 +14,31 @@ import (
 //   成功時：[country_id,name,description, bookmark, weight]の配列
 func GetMapInfo(c *gin.Context) {
 	user, err := User(c)
-	var map_info []models.MapInfo
+	var mapInfo []models.MapInfo
 	if err != nil {
 		// ログインしていない場合
-		database.DB.Table("countries").Scan(&map_info)
+		database.DB.Table("countries").Scan(&mapInfo)
 	} else {
 		// ログイン済みの場合
-		database.DB.Table("countries").Select("countries.id, countries.name, countries.description, results.bookmark, results.weight").Joins("left outer join results on countries.id = results.country_id").Where("results.user_id=?", user.ID).Or("results.user_id IS NULL").Scan(&map_info)
+		var mapInfo1 []models.MapInfo
+		var mapInfo2 []models.MapInfo
+
+		// 該当ユーザーが結果を持つ国
+		query1 := database.DB.Table("countries").Select("countries.id, countries.name, countries.description, results.bookmark, results.weight").Joins("left outer join results on countries.id = results.country_id").Where("results.user_id=?", user.ID).Scan(&mapInfo1)
+
+		// 結果を持たない国
+		// Bookmark, Weightに0を代入
+		query2 := database.DB.Table("countries").Select("countries.id, countries.name, countries.description, 0, 0").Joins("left outer join results on countries.id = results.country_id").Where("countries.id not in (?)", database.DB.Table("results").Select("country_id").Where("user_id=?", user.ID)).Scan(&mapInfo2)
+
+		log.Println("---")
+		log.Println(mapInfo2)
+
+		database.DB.Raw(
+			"? UNION ?",
+			query1,
+			query2,
+		).Scan(&mapInfo)
 	}
 
-	c.JSON(http.StatusOK, gin.H{"map_info": map_info})
+	c.JSON(http.StatusOK, gin.H{"map_info": mapInfo})
 }
